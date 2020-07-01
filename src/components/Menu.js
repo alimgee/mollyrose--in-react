cb1efb85dc9099524a4a8c2166fd8d1f5f0cd014
Alan mc Gee <mcgee.al@gmail.com>
2020-07-01 11:57:32 +0100

    Inital experimental set up and adding nav and footer components using reactstrap

---
+++ src/components/Menu.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  NavbarText
+} from 'reactstrap';
+
+
+const Menu = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar light expand="md">
+        <NavbarBrand href="/">MollyRose</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="/">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/contact">Contact</NavLink>
+            </NavItem>
+            
+          </Nav>
+          <NavbarText>Lets talk about childhood cancer</NavbarText>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Menu;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Route, Link, NavLink, BrowserRouter as Router, Switch} from 'react-router-dom'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Menu from './components/Menu'
+import Footer from './components/Footer'
+import Contact from './components/Contact'
+import './index.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+  const routing = (
+    <Router>
+    <Menu />
+    <Switch>
+          <Route exact path="/" component={App} />
+          <Route path="/contact" component={Contact} />
+          
+        </Switch>
+      <Footer />
+    </Router>
+  )
+  ReactDOM.render(routing, document.getElementById('root'))
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
