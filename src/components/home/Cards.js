a39875c0a9c3913cfabb7f6e5226b054cd951459
Alan mc Gee <mcgee.al@gmail.com>
2020-07-08 11:07:51 +0100

    Updating imagery and cards on home page component

--- src/components/home/Cards.js
+++ src/components/home/Cards.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import img from '../../assets/img/img.png'
-import crumlin from '../../assets/img/crumlin2.jpg'
+import crumlin from '../../assets/img/crumlin2.png'
 import events from '../../assets/img/events.png'
+import ballons from '../../assets/img/cancer-module.png'
 import {
     Container,
     Row,
@@ -22,12 +22,15 @@ const CardSection = (props) => {
                 <Row>
                     <Col md="4" className="mb-4">
                         <Card>
-                            <CardHeader><h3>Featured</h3></CardHeader>
-                            <CardImg variant="top" src={img} className="img-fluid rounded" />
+                            <CardHeader><h3>Tell me more</h3></CardHeader>
+                            <CardImg variant="top" src={ballons} className="img-fluid rounded" />
                             <CardBody>
+                            <div className="card-text" ><h6 style={{fontFamily:"Roboto Mono"}}>Childhood Cancer causes, signs and symptoms</h6></div>
                                 <CardText>
-                                    With supporting text below as a natural lead-in to additional content.</CardText>
-                                <a className="btn" href="/" style={{ textAlign: "left" , marginTop:"11.5rem"  }} role="button">Learn more</a>
+                                    There are many types of diseases that are classed Childhood Cancer. The causes are manifold and in some cases largley unknown.
+                                    However be assured that Childhood Cancer is very rare.
+                                </CardText>
+                                <a className="btn" href="/childhoodcancer/" style={{ textAlign: "left"  }} role="button">Learn more</a>
                             </CardBody>
                         </Card>
                     </Col>
@@ -40,7 +43,7 @@ const CardSection = (props) => {
                                 <CardText >
                                     There are many organisations and charitys that provide much needed support. From assisting the children themselves to 
                                     supporting families affected by a childhood cancer diagnosis</CardText>
-                                <a className="btn" href="/" style={{ textAlign: "left" }} role="button">Learn more</a>
+                                <a className="btn" href="/helpout/" style={{ textAlign: "left" }} role="button">Learn more</a>
                             </CardBody>
                         </Card>
                     </Col>
