000f102538b07f6eba6241751ddf19a3e989284a
Alan mc Gee <mcgee.al@gmail.com>
2020-08-05 13:50:12 +0100

    updating meta og tags

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -11,6 +11,7 @@ const App = () => {
                 <title>Home - mollyrose.ie</title>
                 <meta name="description" content="Driving awareness of Childhood Cancer and Telling Molly Roses story" />
 
+
             </Helmet>
       <Intro />
       <Quote />
