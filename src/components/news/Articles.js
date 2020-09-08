acf6c2498a58996d811752a6d1a51e575a44e9e4
Alan mc Gee <mcgee.al@gmail.com>
2020-09-08 15:34:07 +0100

    Modified layout of news cards

--- src/components/news/Articles.js
+++ src/components/news/Articles.js
@@ -16,11 +16,15 @@ export const Items = () => {
                     <div key={key}>
                         {
                             <Card>
-                                <a href={data.link} target="_blank" rel="noopener noreferrer"  title="Go to news article" style={{ textDecoration: "none" }}>
+                                <a href={data.link} target="_blank" rel="noopener noreferrer" title="Go to news article" style={{ textDecoration: "none" }}>
                                     <CardBody>
                                         <div className="row">
-                                            <div ><CardText><p className="news-header">{data.name} <span className="small muted">(source: {data.provider})</span></p>
-                                                {data.content}</CardText>
+                                            <div >
+                                            <CardText>
+                                                <p className="news-header">{data.name} </p>
+                                                <p className="news-content">{data.content}</p>
+                                                <span className="small muted">(source: {data.provider})</span>
+                                            </CardText>
                                                 <div className="small muted ">{data.date}</div>
                                             </div>
                                             <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="Driving awareness of childhood cancer." data-hashtags="ChildhoodCancer" data-related="mollyrosecancer" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
