5b40fa67953e4ec08be21d2ef05a2c3b4b3ada7e
Alan mc Gee <mcgee.al@gmail.com>
2020-11-11 12:08:19 +0000

    Updated notice

--- src/components/home/Notice.js
+++ src/components/home/Notice.js
@@ -1,36 +1,38 @@
 import React from 'react'
-/* import {
+import {
     Container,
     Row,
     Col,
     CardText,
     Card,
     CardBody,
-} from 'reactstrap'; */
+} from 'reactstrap';
 
 
 const NoticeSection = (props) => {
 
     return (
         <section>
-{/*             <Container className="mb-2">
+            <Container className="mb-2">
                 <Row>
                     <Col>
                         <Card>
                             <CardBody>
                                 <CardText>
-                                <h2>Head Shave Event!!!</h2>
-                                    We had our Head Shave event on the 3rd of September, Mollys 4th anniversary, in 
-                                    order to raise funds for an amazing charity 'Aoibheanns Pink Tie'. If you wish
-                                    to donate to a fantastic cause please do so at the link below. You will also see
-                                    me shorn of my beautiful mane.
+                                <h2>Charity Christmas Cards!!!</h2>
+                                   <p>Its beginning to look a lot like christmas! If you are planning on sending christmas cards this
+                                    year why not send cards that will raise funds for childhood cancer. Here are some online links below
+                                    or you can keep and eye out in  your local bookshops.</p>
+                                    <p><a href="https://www.cardgallery.ie/collections/christmas-cards/products/christmas-card-santas-sleigh-flying-over-town-charity-aoibheanns-pink-tie-6-pack" target="_blank" rel="noopener noreferrer">Aoibheanns Pink Tie</a></p>
+                                    <p><a href="https://www.childreninhospital.ie/orderform/" target="_blank" rel="noopener noreferrer">Children in Hospital</a></p>
+                                    <p><a href="https://rmhc.ie/what-we-do/shop/Christmas-Cards-%E2%80%93-Pack-of-10-%E2%80%93-Includes-Delivery-p251923990" target="_blank" rel="noopener noreferrer">Ronald McDonald House</a></p>
                                 </CardText>
-                                <a title="Donate to Head Shave event" className="btn" href="https://www.idonate.ie/AlanMcGee" style={{ textAlign: "left"  }} role="button" target="_blank" rel="noopener noreferrer">Donate Now!</a>
+                               
                             </CardBody>
                         </Card>
                     </Col>                   
                 </Row>
-            </Container> */}
+            </Container>
         </section>
     )
  
