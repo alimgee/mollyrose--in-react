9264573e0c7d55d6be2efe8161b9d001b8143c13
Alan mc Gee <mcgee.al@gmail.com>
2020-11-11 12:59:59 +0000

    Css update for notice

--- src/components/home/Notice.js
+++ src/components/home/Notice.js
@@ -23,9 +23,10 @@ const NoticeSection = (props) => {
                                    <p>Its beginning to look a lot like christmas! If you are planning on sending christmas cards this
                                     year why not send cards that will raise funds for childhood cancer. Here are some online links below
                                     or you can keep and eye out in  your local bookshops.</p>
-                                    <p><a href="https://www.cardgallery.ie/collections/christmas-cards/products/christmas-card-santas-sleigh-flying-over-town-charity-aoibheanns-pink-tie-6-pack" target="_blank" rel="noopener noreferrer">Aoibheanns Pink Tie</a></p>
-                                    <p><a href="https://www.childreninhospital.ie/orderform/" target="_blank" rel="noopener noreferrer">Children in Hospital</a></p>
-                                    <p><a href="https://rmhc.ie/what-we-do/shop/Christmas-Cards-%E2%80%93-Pack-of-10-%E2%80%93-Includes-Delivery-p251923990" target="_blank" rel="noopener noreferrer">Ronald McDonald House</a></p>
+                                    <p><i class="fa fa-star-o" aria-hidden="true"></i> <a href="https://www.cardgallery.ie/collections/christmas-cards/products/christmas-card-santas-sleigh-flying-over-town-charity-aoibheanns-pink-tie-6-pack" target="_blank" rel="noopener noreferrer">Aoibheanns Pink Tie</a></p>
+                                    <p><i class="fa fa-tree" aria-hidden="true"></i> <a href="https://www.childreninhospital.ie/orderform/" target="_blank" rel="noopener noreferrer">Children in Hospital</a></p>
+                                    <p><i class="fa fa-bell-o" aria-hidden="true"></i> <a href="https://rmhc.ie/what-we-do/shop/Christmas-Cards-%E2%80%93-Pack-of-10-%E2%80%93-Includes-Delivery-p251923990" target="_blank" rel="noopener noreferrer">Ronald McDonald House</a></p>
+                                    
                                 </CardText>
                                
                             </CardBody>
