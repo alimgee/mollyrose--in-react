e6310893ae28f86353eb29a8db2adb24c850462d
Alan mc Gee <mcgee.al@gmail.com>
2021-01-16 13:55:06 +0000

    Adding new notice

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -3,18 +3,19 @@ import Intro from './Intro'
 import Quote from './Quote'
 import CardSection from './Cards'
 import { Helmet } from 'react-helmet'
-//import NoticeSection from './Notice'
+import NoticeSection from './Notice'
 
 const App = () => {
   return (
-      <main>
-        <Helmet>
-                <title>Home - mollyrose.ie</title>
-                <meta name="description" content="Driving awareness of Childhood Cancer and Telling Molly Roses story" />
-            </Helmet>
+    <main>
+      <Helmet>
+        <title>Home - mollyrose.ie</title>
+        <meta name="description" content="Driving awareness of Childhood Cancer and Telling Molly Roses story" />
+      </Helmet>
       <Intro />
       <Quote />
+      <NoticeSection />
       <CardSection />
-      </main>)
+    </main>)
 }
 export default App;
--- src/components/home/Notice.js
+++ src/components/home/Notice.js
@@ -19,24 +19,22 @@ const NoticeSection = (props) => {
                         <Card>
                             <CardBody>
                                 <CardText>
-                                <h2>Charity Christmas Cards!!!</h2>
-                                   <p>Its beginning to look a lot like christmas! If you are planning on sending christmas cards this
-                                    year why not send cards that will raise funds for childhood cancer. Here are some online links below
-                                    or you can keep and eye out in  your local bookshops.</p>
-                                    <p><i class="fa fa-star-o" aria-hidden="true"></i> <a href="https://www.cardgallery.ie/collections/christmas-cards/products/christmas-card-santas-sleigh-flying-over-town-charity-aoibheanns-pink-tie-6-pack" target="_blank" rel="noopener noreferrer">Aoibheanns Pink Tie</a></p>
-                                    <p><i class="fa fa-tree" aria-hidden="true"></i> <a href="https://www.childreninhospital.ie/orderform/" target="_blank" rel="noopener noreferrer">Children in Hospital</a></p>
-                                    <p><i class="fa fa-bell-o" aria-hidden="true"></i> <a href="https://rmhc.ie/what-we-do/shop/Christmas-Cards-%E2%80%93-Pack-of-10-%E2%80%93-Includes-Delivery-p251923990" target="_blank" rel="noopener noreferrer">Ronald McDonald House</a></p>
-                                    
+                                    <h2>Give blood, save a childs life</h2>
+                                    <p>Blood Donations are essential during the treatment of childhood cancer. During lockdown the Irish Blood
+                                    transfusion board continue to need your help.
+                                   </p>
+                                    <p><i class="fa fa-tint mr-2" aria-hidden="true"></i>
+                                        <a href="https://www.giveblood.ie/Find-a-Clinic/Clinic-Finder/" target="_blank" rel="noopener noreferrer">Find a Clinic</a>
+                                    </p>
                                 </CardText>
-                               
                             </CardBody>
                         </Card>
-                    </Col>                   
+                    </Col>
                 </Row>
             </Container>
         </section>
     )
- 
+
 }
 
 export default NoticeSection
