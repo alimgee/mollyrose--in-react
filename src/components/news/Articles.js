b2775491be690b98d871690887fa24f2370d9052
Alan mc Gee <mcgee.al@gmail.com>
2021-01-16 14:55:32 +0000

    Updated archive branch with archive functionality in Article.js

--- src/components/news/Articles.js
+++ src/components/news/Articles.js
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Articles } from "./data";
 import {
     Card,
     CardBody,
     CardText,
+    Collapse,
+    Button
 } from 'reactstrap';
 
+    
+
 export const Items = () => {
+    const [isOpen, setIsOpen] = useState(false); 
+    const toggle = () => setIsOpen(!isOpen);
 
     return (
-
         <div>
             {Articles.map((data, key) => {
                 return (
@@ -20,11 +25,11 @@ export const Items = () => {
                                     <CardBody>
                                         <div className="row">
                                             <div >
-                                            <CardText>
-                                                <p className="news-header">{data.name} </p>
-                                                <p className="news-content">{data.content}</p>
-                                                <span className="small muted">(source: {data.provider})</span>
-                                            </CardText>
+                                                <CardText>
+                                                    <p className="news-header">{data.name} </p>
+                                                    <p className="news-content">{data.content}</p>
+                                                    <span className="small muted">(source: {data.provider})</span>
+                                                </CardText>
                                                 <div className="small muted ">{data.date}</div>
                                             </div>
                                             <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="Driving awareness of childhood cancer." data-hashtags="ChildhoodCancer" data-related="mollyrosecancer" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
@@ -36,6 +41,19 @@ export const Items = () => {
                     </div>
                 );
             })}
+            <div>
+                <Button color="primary" onClick={toggle} style={{ marginBottom: '1.5rem',  padding: '0.5rem 1.5rem 0.5rem  1.5rem' }}>Toggle</Button>
+                <Collapse isOpen={isOpen}>
+                    <Card>
+                        <CardBody>
+                            Anim pariatur cliche reprehenderit,
+                            enim eiusmod high life accusamus terry richardson ad squid. Nihil
+                            anim keffiyeh helvetica, craft beer labore wes anderson cred
+                            nesciunt sapiente ea proident.
+          </CardBody>
+                    </Card>
+                </Collapse>
+            </div>
         </div>
     );
 };
---
+++ src/components/news/archive_data.js
@@ -0,0 +1,227 @@
+export const Articles = [
+  {
+    "name": "Parents 'exploited' by overseas cancer treatments",
+    "content": "Consultant Prof Owen Smith and oncologist Dr Michael Capra of Our Lady’s Hospital in Crumlin, Dublin, said they fear parents of seriously sick children are being “exploited”...........",
+    "link": "https://www.irishtimes.com/news/health/parents-exploited-by-overseas-youth-cancer-treatments-1.1900886",
+    "date": "16th Jan 2021",
+    "provider": "Irish Times"
+  },
+  {
+    "name": "Department of Health can't give timeline for rollout of publicly-funded IVF",
+    "content": "Paul Gordan of the Irish Cancer Society said that “we don’t want to see people who can’t afford IVF not having access to a treatment that is available in most European countries..........",
+    "link": "https://www.thejournal.ie/no-timeline-for-ivf-funding-5302561-Jan2021/",
+    "date": "4th January 2021",
+    "provider": "The Journal"
+  },
+  {
+    "name": "Helping children – and adults – to explore and understand grief",
+    "content": "Stephen Teap would have happily not put up a single decoration for the first Christmas without his wife Irene, but he knew he had to “keep the show on the road”........",
+    "link": "https://www.irishtimes.com/life-and-style/health-family/parenting/helping-children-and-adults-to-explore-and-understand-grief-1.4429912",
+    "date": "16th December 2020",
+    "provider": "Irish Times"
+  },
+  {
+    "name": "Cloud doodles help charities after artist's daughter diagnosed with leukemia",
+    "content": "A Dublin-based artist is using his cloud doodle project to give back to charities who have helped his family through a difficult year...........",
+    "link": "https://www.rte.ie/news/2020/1121/1179622-cloud-doodle-charity/",
+    "date": "23rd November 2020",
+    "provider": "rte.ie"
+  },
+  {
+    "name": "Cork GAA club praised for their 'monumental efforts' after raising almost €94k for charity",
+    "content": "Earlier this week, Aoibheann's Pink Tie took to social media to thank Aghabullogue GAA and everyone else who participated in or donated to the fundraiser..........",
+    "link": "https://www.echolive.ie/corknews/Cork-GAA-club-praised-for-their-monumental-efforts-after-raising-almost-94k-for-childrens-cancer-charity-71d973ab-6e14-4f9c-bb49-44c71e49e520-ds",
+    "date": "5th November 2020",
+    "provider": "echo live"
+  },
+  {
+    "name": "Stars support campaign for Wexford tot Fiadh O’Connor who is battling rare childhood cancer",
+    "content": "THE family of a little girl diagnosed with a rare and aggressive form of cancer are on a mission to raise €375k.........",
+    "link": "https://www.thesun.ie/tvandshowbiz/6066718/amy-huberman-fiadh-oconnor-cancer-treatment-neuroblastoma/",
+    "date": " 27th October 2020",
+    "provider": "The Sun"
+  },
+  {
+    "name": "Watch: Christine Lampard talks about childhood cancer and her worst fears",
+    "content": "She chatted to four mums during Childhood Cancer Awareness Month who have been supported by CLIC Sargent.........",
+    "link": "https://www.belfastlive.co.uk/whats-on/be/watch-christine-lampard-talks-childhood-19074313",
+    "date": "27th October 2020",
+    "provider": "Belfact live"
+  },
+  {
+    "name": "New children's rooms a big help at hospital",
+    "content": "Ciara Galvin speaks to those involved in providing three state-of-the-art rooms at SUH's paediatric ward.........",
+    "link": "https://www.independent.ie/regionals/sligochampion/news/new-childrens-rooms-a-big-help-at-hospital-39643136.html",
+    "date": "24th October 2020",
+    "provider": "Independent.ie"
+  },
+  {
+    "name": "Ryan Tubridy left moved by story of fund raising for child cancer charity",
+    "content": "The Dubliner was clearly touched by the tale of Amelia, Tessa, Emma-Jane and Megan, four eight-year-olds who set out on a readathon to raise money for the Gavin Glynn Foundation.........",
+    "link": "https://www.irishmirror.ie/showbiz/irish-showbiz/ryan-tubridy-left-moved-story-22888327",
+    "date": " 22 Oct 2020",
+    "provider": "Irish Mirror"
+  },
+  {
+    "name": "Parents who lost son to cancer donate fleet of mini garda cars",
+    "content": "A couple who lost one of their twin boys to cancer shortly before he turned two have channelled their grief into fundraising for a fleet of toy garda cars.........",
+    "link": "https://www.irishexaminer.com/news/munster/arid-40058495.html",
+    "date": "2nd Oct 2020",
+    "provider": "Irish Examiner"
+  },
+  {
+    "name": "Trinity cancer research discovery leads to clinical",
+    "content": "A discovery by Trinity College researchers in a form of cancer common in children, has led to the announcement that C4 Therapeutics plan to begin clinical trials in 2021.........",
+    "link": "https://www.eurekalert.org/pub_releases/2020-09/tcd-tcr092820.php",
+    "date": "28th September 2020",
+    "provider": "eureka"
+  },
+  {
+    "name": "Suffolk boy with rare cancer ready for first day at school",
+    "content": "A boy who doctors feared would not survive when he was diagnosed with a rare childhood cancer aged four months is due to start school..........",
+    "link": "https://www.bbc.com/news/uk-england-suffolk-54239800",
+    "date": "21st September 2020",
+    "provider": "BBC news"
+  },
+  {
+    "name": "Child cancer room opened at Lourdes",
+    "content": "Thanks to the joint efforts of Drogheda Rotary Club and The Childhood Cancer Foundation, the first little patients are now using the dedicated paediatric oncology room.........",
+    "link": "https://www.independent.ie/regionals/droghedaindependent/news/child-cancer-room-opened-at-lourdes-39510384.html",
+    "date": "12th September 2020",
+    "provider": "Independent.ie"
+  },
+  {
+    "name": "Irish Cancer Society - Marathon in a month",
+    "content": "The marathon EVERYONE can complete! You can walk, run or jog it, at your own speed and in your own time, but the main thing is you CAN do it, you CAN complete a marathon!.........",
+    "link": "https://marathoninamonth.cancer.ie/",
+    "date": "11th September 2020",
+    "provider": "Irish Cancer Society"
+  },
+  {
+    "name": "Sophie (5) lit up a room and was a little inspiration",
+    "content": "Sophie Walsh, who left this world on Tuesday (18th) aged five, was a ray of sunshine who brightened up the lives of everyone.........",
+    "link": "https://www.independent.ie/regionals/newrossstandard/sophie-5-lit-up-a-room-and-was-a-little-inspiration-39474814.html",
+    "date": "29 August 2020",
+    "provider": "Independent.ie"
+  },
+  {
+    "name": "Sheridan Smith's tragic childhood trauma after older brother died from cancer",
+    "content": "Sheridan Smith grew teary eyed as she detailed her brother's battle with cancer on her new documentary..........",
+    "link": "https://www.irishmirror.ie/tv/sheridan-smiths-tragic-childhood-trauma-22614248",
+    "date": "1st September 2020",
+    "provider": "Irish Mirror"
+  },
+  {
+    "name": "Chadwick Boseman helped children with cancer while keeping own diagnosis private",
+    "content": "Chadwick Boseman kept his cancer diagnosis private while “bringing joy and courage” to children with the disease..........",
+    "link": "https://www.irishexaminer.com/lifestyle/celebrity/arid-40039847.html",
+    "date": " August 29th 2020",
+    "provider": "Irish Examiner"
+  },
+  {
+    "name": "Family plant cherry tree in memory of heroic Robyn",
+    "content": "A cherry tree has been planted in memory of a Dublin teenager who battled a rare cancer for most of her life. Robyn Smyth, who suffered from neuroblastoma, captured the hearts of the nation...........",
+    "link": "https://www.herald.ie/news/family-plant-cherry-tree-in-memory-of-heroic-robyn-39471916.html",
+    "date": "24th August 2020",
+    "provider": "The Herald"
+  },
+  {
+    "name": "Evidence for hypoxia increasing the tempo of evolution in glioblastoma",
+    "content": "Our results suggest that hypoxia could be a crucible that effectively warps evolutionary velocity, making key mutations more likely. ...........",
+    "link": "https://www.nature.com/articles/s41416-020-1021-5",
+    "date": "27th August 2020",
+    "provider": "nature.com (BJC)"
+  },
+  {
+    "name": "Initiative created to protect the fertility of children",
+    "content": "Every year in Ireland, more than 200 children are diagnosed with cancer, and the necessary treatment they are subscribed can often end up damaging their fertility........",
+    "link": "https://www.herfamily.ie/wellness/new-initiative-created-protect-future-fertility-children-young-people-treated-cancer-379633",
+    "date": "20th August 2020",
+    "provider": "herfamily.ie"
+  },
+  {
+    "name": "September is Childhood Cancer Awareness Month ",
+    "content": "Sadly, every week in the UK, two families are told that their child has neuroblastoma, a rare and life-threatening cancer. Lets work together to drive awareness of childhood cancer........",
+    "link": "https://www.neuroblastoma.org.uk/childhood-cancer-awareness-month?gclid=CjwKCAjw1ej5BRBhEiwAfHyh1GjOgykHqHVLMbBgItn4R3RsEPbWCfATT1dFhec5GjM9-JW_KLpnchoCM0sQAvD_BwE",
+    "date": "17th August 2020",
+    "provider": "neuroblastoma.org.uk"
+  },
+  {
+    "name": "Share A Dream founder Shay Kinsella will be remembered ",
+    "content": "Share A Dream Foundation founder Shay Kinsella was remembered at his funeral mass as a “dream maker” who helped thousands of vulnerable children........",
+    "link": "https://www.irishmirror.ie/news/irish-news/shay-kinsella-share-a-dream-22437392",
+    "date": "29th July 2020",
+    "provider": "The Mirror"
+  },
+  {
+    "name": "Preserving Childhood Cancer Care Amid a Pandemic",
+    "content": "A survey in 19 countries shows that coordination and messaging at the national level is essential to maintain critical cancer treatment, an expert says.........",
+    "link": "https://www.usnews.com/news/best-countries/articles/2020-08-04/study-coronavirus-response-delayed-some-childhood-cancer-treatments",
+    "date": "4th Aug 2020",
+    "provider": "USNews"
+  },
+  {
+    "name": "Ruth Morrissey: farewell to cervical cancer pioneer",
+    "content": "Ruth Morrissey, who campaigned for Cervical Check, was a “beautiful soul” who showed great resilience and strength in her final years, her funeral was told yesterday.......",
+    "link": "https://www.thetimes.co.uk/article/ruth-morrissey-farewell-to-incredibly-courageous-cervical-cancer-pioneer-v2l8fmtr6",
+    "date": "23rd July 2020",
+    "provider": "The Times"
+  },
+  {
+    "name": "€94k is raised for children's cancer charity",
+    "content": "Cork parents, whose firstborn son was diagnosed with a rare childhood cancer in 2018, have moved to thank the public for generously supporting a fundraiser.......",
+    "link": "https://www.echolive.ie/corknews/Cork-parents-issue-heartfelt-thanks-after-almost-94k-is-raised-for-childrens-cancer-charity-2bb9792a-96d8-41d3-a60a-7f2b726e0748-ds",
+    "date": "20th July 2020",
+    "provider": "www.echolive.ie"
+  },
+  {
+    "name": "Irish Cancer Society Colour Dash Family 5k",
+    "content": "Due to the current coronavirus pandemic, Colour Dash as we know and love it can no longer go ahead. Instead we are inviting you and your family to take part in the first 5k.......",
+    "link": "https://www.cancer.ie/ways-to-help/fundraise/colour-dash",
+    "date": "5th July 2020",
+    "provider": "Irish Cancer Society"
+  },
+  {
+    "name": "Get on your bike for children’s cancer charity",
+    "content": "An Irish children’s cancer charity is calling on people to get on their bikes this summer to help ensure that no child in Ireland has to face cancer alone......",
+    "link": "https://www.rte.ie/brainstorm/2020/0625/1149549-children-leukaemia/",
+    "date": "16th June 2020",
+    "provider": "Connaught telegraph"
+  },
+  {
+    "name": "Why do children get leukaemia?",
+    "content": "Leukaemia is a type of blood cancer that affects both white blood cells, the key players in the body's defence against infection, and bone marrow, the spongy soft centre of the bone where blood cells are made.....",
+    "link": "https://www.rte.ie/brainstorm/2020/0625/1149549-children-leukaemia/",
+    "date": "26th June 2020",
+    "provider": "RTE"
+  },
+  {
+    "name": "Mayo has highest cancer rate in Ireland over the past decade",
+    "content": "Our latest data investigation reveals the western county has held our nation’s highest overall cancer rate over the past decade.Mayo also has the worst rates of five of the most common and deadliest cancers.......",
+    "link": "https://www.rte.ie/brainstorm/2020/0625/1149549-children-leukaemia/",
+    "date": "17th June 2020",
+    "provider": "Irish Mirror"
+  },
+  {
+    "name": "Genetics Could Help ID Childhood Cancer Survivors at Risk for New Malignancies",
+    "content": "Analyzing the combination of treatments that childhood cancer survivors received for their malignancies as well as inherited mutations in their DNA-repair genes can help predict their risk for new cancer diagnoses......",
+    "link": "https://www.cancerhealth.com/article/genetics-help-childhood-cancer-survivors-risk-new-malignancies",
+    "date": "17th June 2020",
+    "provider": "cancerhealth.com"
+  },
+  {
+    "name": "Virtual event breaks world record for charity 'strip and dip",
+    "content": "An event raising money for childhood cancer has broken an official Guinness World Record for the 'most photos of people in water uploaded to Facebook in one hour', as part of a fundraising 'Strip and Dip' drive.....",
+    "link": "https://www.rte.ie/news/ireland/2020/0606/1145885-strip-and-dip/",
+    "date": "6th June 2020",
+    "provider": "RTE"
+  },
+  {
+    "name": "Significant decline in childhood cancer mortality",
+    "content": "A new report from the National Cancer Registry shows that childhood cancer mortality has declined over the past 50 years....",
+    "link": "https://www.rte.ie/news/2017/0228/855959-cancer/",
+    "date": "28th Feb 2020",
+    "provider": "RTE"
+  }
+]
+
