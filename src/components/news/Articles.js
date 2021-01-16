8b00d026a1eef1a6483f16a8ea5104edd7552ccf
Alan mc Gee <mcgee.al@gmail.com>
2021-01-16 16:16:49 +0000

    Styled view archive button

--- src/components/news/Articles.js
+++ src/components/news/Articles.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Articles } from "./data";
-import {ArchivedArticles} from "./archive_data"
+import { ArchivedArticles } from "./archive_data"
 import {
     Card,
     CardBody,
@@ -21,28 +21,28 @@ export const Items = () => {
                     <div key={key}>
                         {
                             <Card>
-                                
-                                    <CardBody>
 
-                                            <div >
-                                                <div className = "card-text">
-                                                <a href={data.link} target="_blank" rel="noopener noreferrer" title="Go to news article" style={{ textDecoration: "none" }}><div className="news-header">{data.name} </div></a>
-                                                    <div className="news-content">{data.content}</div>
-                                                    <span className="small muted">(source: {data.provider})</span>
-                                                </div>
-                                                <div className="small muted ">{data.date}</div>
-                                            </div>
-                                            <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" className="twitter-share-button" data-text="Driving awareness of childhood cancer." data-hashtags="ChildhoodCancer" data-related="mollyrosecancer" data-show-count="false">Tweet</a>
-                                            <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+                                <CardBody>
 
-                                    </CardBody>
+                                    <div >
+                                        <div className="card-text">
+                                            <a href={data.link} target="_blank" rel="noopener noreferrer" title="Go to news article" style={{ textDecoration: "none" }}><div className="news-header">{data.name} </div></a>
+                                            <div className="news-content">{data.content}</div>
+                                            <span className="small muted">(source: {data.provider})</span>
+                                        </div>
+                                        <div className="small muted ">{data.date}</div>
+                                    </div>
+                                    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" className="twitter-share-button" data-text="Driving awareness of childhood cancer." data-hashtags="ChildhoodCancer" data-related="mollyrosecancer" data-show-count="false">Tweet</a>
+                                    <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+
+                                </CardBody>
                             </Card>
                         }
                     </div>
                 );
             })}
             <div>
-                <Button  onClick={toggle} style={{ marginBottom: '1.5rem', padding: '0.5rem 1.5rem 0.5rem  1.5rem' }}>Toggle</Button>
+                <Button onClick={toggle} style={{ marginBottom: '1.5rem', padding: '0.5rem 1.5rem 0.5rem  1.5rem' }}>View Archived Articles <i className="fa fa-arrow-right" aria-hidden="true"></i></Button>
                 <Collapse isOpen={isOpen}>
                     <div>
                         {ArchivedArticles.map((data, key) => {
@@ -50,21 +50,21 @@ export const Items = () => {
                                 <div key={key}>
                                     {
                                         <Card>
-                                            
-                                                <CardBody>
-                                                    <div className="row">
-                                                        <div >
-                                                        <div className = "card-text">
-                                                        <a href={data.link} target="_blank" rel="noopener noreferrer" title="Go to news article" style={{ textDecoration: "none" }}> <div className="news-header">{data.name} </div> </a>
-                                                    <div className="news-content">{data.content}</div>
-                                                    <span className="small muted">(source: {data.provider})</span>
-                                                </div>
-                                                            <div className="small muted ">{data.date}</div>
+
+                                            <CardBody>
+                                                <div className="row">
+                                                    <div >
+                                                        <div className="card-text">
+                                                            <a href={data.link} target="_blank" rel="noopener noreferrer" title="Go to news article" style={{ textDecoration: "none" }}> <div className="news-header">{data.name} </div> </a>
+                                                            <div className="news-content">{data.content}</div>
+                                                            <span className="small muted">(source: {data.provider})</span>
                                                         </div>
-                                                        <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" className="twitter-share-button" data-text="Driving awareness of childhood cancer." data-hashtags="ChildhoodCancer" data-related="mollyrosecancer" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+                                                        <div className="small muted ">{data.date}</div>
                                                     </div>
-                                                </CardBody>
-                                            
+                                                    <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" className="twitter-share-button" data-text="Driving awareness of childhood cancer." data-hashtags="ChildhoodCancer" data-related="mollyrosecancer" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
+                                                </div>
+                                            </CardBody>
+
                                         </Card>
                                     }
                                 </div>
